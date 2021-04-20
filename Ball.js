d3031edbe85aeb1979da48e126658d865d962555
abhyudaya726 <71880557+abhyudaya726@users.noreply.github.com>
2021-04-20 14:43:39 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,22 +1,31 @@
-class Ball{
-    constructor(x,y,radius){
-
-        var options = {
-            isStatic:false,
-            restitution:0.3,
-            friction:0.5,
-            density:2.0
-        }
-
-        this.bodies = Bodies.circle(x,y,radius,options);
-        World.add(world,this.bodies);
-
-        this.r = radius;
+class Paper 
+{
+  constructor(x, y,radius) 
+  {
+    var options = 
+    {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.2
     }
-    display(){
-        var pos = this.bodies.position;
-
-        ellipseMode(CENTER);
-        ellipse(pos.x,pos.y,this.r);
-    }
-}
+    this.body = Bodies.circle(x, y, radius, options);
+    this.radius = radius;
+    
+    
+    World.add(world, this.body);
+  }
+  display()
+  {
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(CENTER);
+    strokeWeight(4);
+    stroke("blue");
+    fill(255);
+    ellipse(0, 0, this.radius);
+    pop();
+  }
+};
