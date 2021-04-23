9d22c2214bc9817f5604531fa17764679af80d72
abhyudaya726 <71880557+abhyudaya726@users.noreply.github.com>
2021-04-23 15:24:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,28 +10,32 @@ function preload()
 
 function setup() 
 {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
 
-	ground = new Ground(600,height,1200,20)
-	D1= new Dustbin(600,680,200,20);
-	D2=new Dustbin(500,615,20,150);
-    D3=new Dustbin(700,615,20,150);
+	ground = new Ground(800,690,1600,30)
+	D1= new Dustbin(1300,665,170,20);
+	D2=new Dustbin(1205,615,20,120);
+    D3=new Dustbin(1395,615,20,120);
 
    
-	paper= new Paper (100,600,50);
+	paper= new Paper (100, 360, 20);
 
-	Engine.run(engine);
   
 }
 
 
 function draw() 
 {
+	
+  background("lightSkyBlue");
+	
+  Engine.update(engine);
+	
   rectMode(CENTER);
   background(0);
 
